fcca2ccfa8805562f611111a7486b261297f8f59
SuWuXBDJ <94089248+XBDJ504764827@users.noreply.github.com>
2024-06-20 13:25:08 +0800

    Update rtlog.sh

--- rtlog.sh
+++ rtlog.sh
@@ -2,6 +2,6 @@
 
 python3 /home/csgo/a2s_restart.py > /home/csgo/log/server_reboot/text.tx
 
-current_date_time=$(date '+%Y-%m-%d-%H-%M-%S')
+current_date_time=$(date '+%Y-%m-%d-%H:%M:%S')
 
 mv /home/csgo/log/server_reboot/text.txt /home/csgo/log/server_reboot/reboot-${current_date_time}.txt
